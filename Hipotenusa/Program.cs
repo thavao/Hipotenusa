9a9c6351a1e88b7326ef6238ea2fd1fbbf6c7dbf
thavao <othavioact@gmail.com>
2024-04-20 21:30:10 -0300

    Primeiro commit

---
+++ Hipotenusa/Program.cs
@@ -0,0 +1,20 @@
+﻿double cateto1, cateto2, hipotenusa;
+
+Console.WriteLine("Digite o valor do primeiro cateto");
+cateto1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o valor do primeiro cateto");
+cateto2 = double.Parse(Console.ReadLine());
+
+if (cateto1 > 0 && cateto1 > 0)
+{
+    hipotenusa = Math.Sqrt(Math.Pow(cateto1, 2) + Math.Pow(cateto2, 2));
+
+    Console.WriteLine("A hipotenusa é: " + hipotenusa);
+}
+else
+    Console.WriteLine("Um dos valores digitados é inválido. Tente novamente...");
+
+Console.WriteLine("Digite Enter para encerrar...");
+
+Console.ReadLine();
